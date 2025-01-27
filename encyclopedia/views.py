30634a2385f45d1598f810881aed9508adb0dfcb
“Dankyi <dankyibenjamin0@gmail.com>
2025-01-27 17:14:29 -0500

    entry page with url routing for titles and markup convertion to html

--- encyclopedia/views.py
+++ encyclopedia/views.py
@@ -1,6 +1,7 @@
 from django.shortcuts import render
 
 from . import util
+import markdown2
 
 
 def index(request):
@@ -8,3 +9,21 @@ def index(request):
         "entries": util.list_entries()
     })
 
+# retrieve the content of the entry
+def entry_pages(request, title):
+    content = util.get_entry(title)
+#    if the entry is not found, return an error message
+    if content:
+        # converting into html
+        html_content = markdown2.markdown(content)
+        return render(request, "encyclopedia/entry.html", {
+            "title": title,
+            "content": html_content
+        })
+    else:
+        return render(request, "encyclopedia/error.html", {
+            "message": "The requested page was not found.",
+            "title": title
+        })
+    
+# work on the search view
